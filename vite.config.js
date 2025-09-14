7da69eac291071dce012a78451391eb34d357e84
Karthik243011 <Karthick.kutty30@gmail.com>
2025-09-14 14:59:58 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/myapp/',   // 👈 must match your repo name
 })
